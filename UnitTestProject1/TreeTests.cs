bf4fd48ef863d472f30229da8ec60258d0ec8088
Kurkulya <kulinich_v@hotmail.com>
2017-08-31 03:54:56 +0300

    Added BsTreeC

--- UnitTestProject1/TreeTests.cs
+++ UnitTestProject1/TreeTests.cs
@@ -7,6 +7,7 @@ namespace UnitTestProject1
     [TestFixture(typeof(BsTree))]
     [TestFixture(typeof(BsTreeV))]
     [TestFixture(typeof(BsTreeR))]
+    [TestFixture(typeof(BsTreeC))]
     public class NUnitTests<TTree> where TTree : ITree, new()
     {
         ITree lst = new TTree();
